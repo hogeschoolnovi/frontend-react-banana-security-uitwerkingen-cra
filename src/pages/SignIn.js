9367d5d7ce6234688c102c14e78f04e49d3a7b93
novaeeken <nova_eeken@hotmail.com>
2021-10-07 11:38:18 +0200

    Implement form-fields with react-hook-form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import { useForm } from 'react-hook-form';
 
 function SignIn() {
   const { login } = useContext(AuthContext);
+  const { handleSubmit, register } = useForm();
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  function onSubmit(data) {
+    console.log(data);
     login();
   }
 
@@ -15,9 +17,30 @@ function SignIn() {
       <h1>Inloggen</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
 
-      <form onSubmit={handleSubmit}>
-        <p>*invoervelden*</p>
-        <button type="submit">Inloggen</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email-field">
+          Emailadres:
+          <input
+            type="email"
+            id="email-field"
+            {...register("email")}
+          />
+        </label>
+
+        <label htmlFor="password-field">
+          Wachtwoord:
+          <input
+            type="password"
+            id="password-field"
+            {...register("password")}
+          />
+        </label>
+        <button
+          type="submit"
+          className="form-button"
+        >
+          Inloggen
+        </button>
       </form>
 
       <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 function SignUp() {
+  const { handleSubmit, register } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <>
       <h1>Registreren</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque eligendi
         harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur deserunt
         doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
-      <form>
-        <p>*Invoervelden*</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email-field">
+          Email:
+          <input
+            type="email"
+            id="email-field"
+            {...register("email")}
+          />
+        </label>
+
+        <label htmlFor="username-field">
+          Gebruikersnaam:
+          <input
+            type="text"
+            id="username-field"
+            {...register("username")}
+          />
+        </label>
+
+        <label htmlFor="password-field">
+          Wachtwoord:
+          <input
+            type="password"
+            id="password-field"
+            {...register("password")}
+          />
+        </label>
+        <button
+          type="submit"
+          className="form-button"
+        >
+          Maak account aan
+        </button>
       </form>
       <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
     </>
