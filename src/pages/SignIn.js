7e8ec8611d6eceb4f90a2728e4eaf6ef22815b4a
novaeeken <nova_eeken@hotmail.com>
2021-10-07 11:25:02 +0200

    Add form-fields

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,12 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const { login } = useContext(AuthContext);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    console.log({
+      email: email,
+      wachtwoord: password,
+    });
+
     login();
   }
 
@@ -16,8 +25,34 @@ function SignIn() {
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
 
       <form onSubmit={handleSubmit}>
-        <p>*invoervelden*</p>
-        <button type="submit">Inloggen</button>
+        <label htmlFor="email-field">
+          Emailadres:
+          <input
+            type="email"
+            id="email-field"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <label htmlFor="password-field">
+          Wachtwoord:
+          <input
+            type="password"
+            id="password-field"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="form-button"
+        >
+          Inloggen
+        </button>
       </form>
 
       <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
