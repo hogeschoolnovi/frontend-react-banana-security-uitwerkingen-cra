f4020f629907a6a0704df224d57234f909178da0
novaeeken <nova_eeken@hotmail.com>
2021-10-07 11:03:25 +0200

    Implement solutions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '../assets/banana-01.png';
 import { useHistory, Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 function NavBar() {
+  const { isAuth, logout } = useContext(AuthContext);
   const history = useHistory();
 
   return (
     <nav>
-        <Link to="/">
+      <Link to="/">
           <span className="logo-container">
             <img src={logo} alt="logo"/>
             <h3>
               Banana Security
             </h3>
           </span>
-        </Link>
+      </Link>
 
-      <div>
+      {isAuth ?
         <button
           type="button"
-          onClick={() => history.push('/signin')}
+          onClick={logout}
         >
-          Log in
+          Log uit
         </button>
-        <button
-          type="button"
-          onClick={() => history.push('/signup')}
-        >
-          Registreren
-        </button>
-      </div>
+        :
+        <div>
+          <button
+            type="button"
+            onClick={() => history.push('/signin')}
+          >
+            Log in
+          </button>
+          <button
+            type="button"
+            onClick={() => history.push('/signup')}
+          >
+            Registreren
+          </button>
+        </div>
+      }
     </nav>
   );
 }
